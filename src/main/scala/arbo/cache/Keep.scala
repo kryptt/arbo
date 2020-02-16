8af3ca7bd95ef5e5e3b745c0a2c472b07676e53d
Rodolfo Hansen <rodolfo.hansen@philips.com>
2020-02-16 03:38:00 +0100

    Small cache implementation

---
+++ src/main/scala/arbo/cache/Keep.scala
@@ -0,0 +1,35 @@
+package arbo
+package cache
+
+import cats.effect.Async
+import cats.effect.concurrent.{Semaphore, Ref}
+
+import cats.syntax.apply._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+
+import scala.concurrent.duration._
+
+object Keep {
+
+  def keep[F[_]: Async, A](time: FiniteDuration)(fa: F[A]): F[F[A]] = {
+
+    def eval: F[(Long, A)] = fa.map(a => (System.nanoTime(), a))
+
+    def maintain(ref: Ref[F, (Long, A)]): F[A] = ref.get.flatMap {
+      case (tm, _) if tm + time.toNanos < System.nanoTime() =>
+        eval.flatTap(ref.set).map(_._2)
+      case (_, a) =>
+        Async[F].pure(a)
+    }
+
+    def run(sem: Semaphore[F], ref: Ref[F, (Long, A)]): F[A] =
+      sem.withPermit(maintain(ref))
+
+    val sem = Semaphore.uncancelable[F](1)
+    val ref = eval.flatMap(ini => Ref.of(ini))
+
+    (sem, ref).mapN(run)
+  }
+
+}
